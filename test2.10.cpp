f9a5ad22f68bbd15f29586a25b7bbd9d8be1cecf
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-08 20:41:47 +0800

    提交test2.10

---
+++ test2.10.cpp
@@ -0,0 +1,19 @@
+/**=================================================================================================
+ * @file	test2.10.cpp.
+ *
+ * Implements the test 2.10 class
+ * 下列变量的初值分别是什么
+ * int global_int初值为0，其他均无初值,local_int没有初值无法打印
+ *===============================================================================================**/
+#include<iostream>
+std::string  global_str;
+int global_int;
+
+int main() {
+	int local_int;
+	std::string local_str;
+	std::cout << global_str << std::endl;
+	std::cout << global_int << std::endl;
+	//std::cout << local_int << std::endl;
+	std::cout << local_str << std::endl;
+}
