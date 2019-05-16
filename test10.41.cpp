72fcba39dbc51c82fded648adcf70f25c116a36a
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-16 19:53:14 +0800

    提交chapter10题目

---
+++ test10.41.cpp
@@ -0,0 +1,10 @@
+/**=================================================================================================
+ * @file	test10.41.cpp.
+ *
+ * Implements the test 10.41 class
+ * 练习 10.41: 仅根据算法和参数的名字， 描述下面每个标准痄算法执行什么操作:
+ * replace(beg, end, old_val, new_val);
+ * replace_if(beg, end, pred, new_val);
+ * replace_copy(beg, end, dest, old_val, new_val);
+ * replace_copy_if(beg, end, dest, pred, new_val);
+ *===============================================================================================**/
