6364f013544c8dcea8d00add8a66f8aa04aa0ae2
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-16 20:45:45 +0800

    提交chapter11题目

---
+++ test11.17.cpp
@@ -0,0 +1,10 @@
+/**=================================================================================================
+ * @file	test11.17.cpp.
+ *
+ * Implements the test 11.17 class
+ * 练习 11.17: 假定 c 是一个 string 的 multiset， v 是一个 string 的 vector, 解释下面的调用。 指出每个调用是否合法：
+ * copy(v.begin(), v.end(), inserter(c, c.end()));
+ * copy(v.begin(), v.end(), back_inserter(c));
+ * copy(c.begin(), c.end(), inserter(v, v.end()));
+ * copy(c.begin(), c.end(), back_inserter(v));
+ *===============================================================================================**/
