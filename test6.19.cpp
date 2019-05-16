60f3d6214addf93d950011af572c2668bd79020f
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-16 16:21:21 +0800

    提交Chapter6题目

---
+++ test6.19.cpp
@@ -0,0 +1,14 @@
+/**=================================================================================================
+ * @file	test6.19.cpp.
+ *
+ * Implements the test 6.19 class
+ * 练习 6.19: 假定有如下声明， 判断哪个调用合法、 哪个调用不合法。 对于不合法的函数调用， 说明原因。
+ * double calc(double);
+ * int count(const string &, char);
+ * int sum(vector<int>::iterator, vector<int>::iterator, int);
+ * vector<int> vec(10);
+ * (a)calc(23.4, 55.1);
+ * (c)calc ( 66 );
+ * (b)count("abcda", r ar );
+ * (d)sum(vec.begin(), vec.end(), 3.8);
+ *===============================================================================================**/
---
+++ test6.53.cpp
@@ -0,0 +1,11 @@
+/**=================================================================================================
+ * @file	test6.53.cpp.
+ *
+ * Implements the test 6.53 class
+ * 练习 6.53： 说明下列每组声明中的第二条语句会产生什么影响, 并指出哪些不合法（ 如果有的话）。
+ * (a) int calc(int&, int&);
+ * int calc(const int&, const int&);
+ * (b) int calc(char*, char*);
+ * int calc(const char*, const char*);
+ * (c) int int calc calc( (char char**,const char”, ；char* const);
+ *===============================================================================================**/
