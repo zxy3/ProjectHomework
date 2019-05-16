55cdf435b6a1b07d517236c2b41c259eaa3396c3
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-16 15:32:46 +0800

    提交chapter 题目 提交chapter 前10道题目

---
+++ test5.18.cpp
@@ -0,0 +1,19 @@
+/**=================================================================================================
+ * @file	test5.18.cpp.
+ *
+ * Implements the test 5.18 class
+ * 练习 5.18： 说明下列循环的含义并改正其中的错误。
+ * (a)do  int vl, v2;
+ * cout << "Please enter two numbers to s u m:" ;
+ * if (cin >> vl >> v2)
+ * cout<< "Sum is: " << vl + v2 << endl;
+ * while (cin);
+ * 
+ * (b)do {
+ * // ...
+ * } while (int ival = get_response());
+ * 
+ * (c)do {
+ * int ival = get_response();
+ * } while (ival);
+ *===============================================================================================**/
---
+++ test5.7.cpp
@@ -0,0 +1,18 @@
+﻿/**=================================================================================================
+ * @file	test5.7.cpp.
+ *
+ * Implements the test 5.7 class
+ * 练习 5.7: 改正下列代码段中的错误。
+ * (a)if (ival1!= ival2) 
+ * ival2=ival1;
+ * else ivall = ival2 = 0;
+ * (b)if (ival < minval)
+ * minval = ival;
+ * occurs = 1;
+ * (c)if (int ival = get_value())
+ * cout << "ival=" << ival << endl;
+ * if (lival)
+ * cout « "ival = 0\n";
+ * (d) if (ival = 0)
+ * ival = get_value();
+ *===============================================================================================**/
