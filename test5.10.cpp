db0aecc1167259aa6080f4131c0e5331a4db2f13
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-17 15:46:10 +0800

    提交test5.1-test5.10

--- test5.10.cpp
+++ test5.10.cpp
@@ -2,6 +2,50 @@
  * @file	test5.10.cpp.
  *
  * Implements the test 5.10 class
- * 练习 5.10: 我们之前实现的统计元音字母的程序存在一个问题： 如果元音字母以人写形式出现， 不会被统计在内。
- *  编写一段程序， 既统计元音字母的小写形式， 也统计大写形式， 也 就 是 说， 新 程 序 遇 到 和'A'都应该递增 aCnt 的值， 以此类推。
+ * 练习 5.10: 我们之前实现的统计元音字母的程序存在一个问题： 如果元音字母以大写形式出现， 不会被统计在内。
+ *  编写一段程序， 既统计元音字母的小写形式， 也统计大写形式， 也就是说，新程序遇和'A'都应该递增 aCnt 的值， 以此类推。
+ *  字符或者字符串，命令行结束输入用什么？
  *===============================================================================================**/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	char cval;
+	int char_a = 0, char_e = 0, char_i = 0, char_o = 0, char_u = 0;
+	while (cin >> cval)
+	{
+		switch (cval)
+		{
+		case 'a':
+		case 'A':
+			++char_a;
+			break;
+		case 'e':
+		case 'E':
+			++char_e;
+			break;
+		case 'i':
+		case 'I':
+			++char_i;
+			break;
+		case 'o':
+		case 'O':
+			++char_o;
+			break;
+		case 'u':
+		case 'U':
+			++char_u;
+			break;
+		}
+	}
+
+	cout << "a或A：" <<" "<<  char_a << endl;
+	cout << "e或E：" <<" "<< char_e << endl;
+	cout << "i或I：" << " " << char_i << endl;
+	cout << "o或O：" << " " << char_o << endl;
+	cout << "u或U：" << " " << char_u << endl;
+
+	return 0;
+}
--- test5.7.cpp
+++ test5.7.cpp
@@ -5,14 +5,20 @@
  * 练习 5.7: 改正下列代码段中的错误。
  * (a)if (ival1!= ival2) 
  * ival2=ival1;
- * else ivall = ival2 = 0;
+ * else ival1= ival2 = 0;
+ * 错误：不能知道ival1和 ival2是什么类型，不能直接赋值0.如果不是int类型的变量就会报错，!=可以用来对比多种类型的变量，不仅仅是int类型。
  * (b)if (ival < minval)
  * minval = ival;
  * occurs = 1;
+ * 错误：块要用花括号
  * (c)if (int ival = get_value())
  * cout << "ival=" << ival << endl;
- * if (lival)
- * cout « "ival = 0\n";
+ * if (!ival)
+ * cout <<"ival = 0\n";
+ * 错误：get_value()返回的数值是否为int类型的值不能确定。
+ *           ival不是一个bool类型的变量，不能给出true或者flase的判断。
  * (d) if (ival = 0)
  * ival = get_value();
+ * 错误：get_value()返回什么类型的值不能确定
+ * ival = 0意思将0赋值给ival，而不是判断ival是不是0，要用==
  *===============================================================================================**/
