aef8586c6f962b503e53521cd539a3d92ffcd910
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-20 22:40:57 +0800

    提交test6.31-test6.40

--- test6.33.cpp
+++ test6.33.cpp
@@ -4,3 +4,24 @@
  * Implements the test 6.33 class
  * 练习 6.33: 编写一个递归函数， 输出 vector 对象的内容。
  *===============================================================================================**/
+#include <iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+void Get_val(vector<int> a, int ix)//函数的声明及定义，接受两个参数
+{
+	if (ix != -1)
+	{
+		cout << a[ix] << endl;
+		ix--;
+		Get_val(a, ix);//递归
+	}
+}
+int main(int argc, char* argv[])
+{
+	int a[10] = { 2,2,3,6,5,4,7,8,55,5 };
+	vector<int> Example(a, a + 10);
+	Get_val(Example, Example.size() - 1);//注意下标从0开始
+	return 0;//代表执行成功
+}
--- test6.37.cpp
+++ test6.37.cpp
@@ -4,4 +4,19 @@
  * Implements the test 6.37 class
  * 练习 6.37: 为 L一题的函数再写三个声明， 
  * 一个使用类型別名， 另一个使用尾置返冋类型， 最后一个使用 decltype 关键字。 你觉得哪种形式最好？ 为什么？
+ *  
+ *  
  *===============================================================================================**/
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+	using ArrT = string[10];
+	ArrT& func1(ArrT & arr);//使用类型别名
+
+	auto func2(ArrT & arr)->string(&)[10]; //使用尾置返回类型
+
+	string arrS[10];
+	decltype(arrS)& func3(ArrT & arr);//使用decltype关键字
+
+}
