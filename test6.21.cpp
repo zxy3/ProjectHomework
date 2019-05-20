ad68c13d9d15cc620c56c38755f049959a619486
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-20 20:33:57 +0800

    提交test6.21-test6.22

--- test6.21.cpp
+++ test6.21.cpp
@@ -3,5 +3,24 @@
  *
  * Implements the test 6.21 class
  * 练习 6.21: 编写一个函数， 令其接受两个参数： 一个是 int 型的数， 另一个是 int 指针。 
- * 函数比较 int 的值和指针所指的值， 返回较大的那个。 在该函数中指针的类型应该是什么？
+ * 函数比较 int 的值和指针所指的值， 返回较大的那个。 
+ * 在该函数中指针的类型应该是什么？ int型
  *===============================================================================================**/
+#include<iostream>
+#include<string>
+using namespace std;
+int selectLarge(int num1, int* num2) {
+	if (num1 > * num2) {
+		return num1;
+	}
+	else {
+		return *num2;
+	}
+}
+int main() {
+	cout << "请输入2个整数！" << endl;
+	int num1, num2;
+	int* num3 = &num2;
+	cin >> num1 >> num2;
+	cout <<"较大的数是："<< selectLarge(num1, num3)<<endl;
+}
--- test6.22.cpp
+++ test6.22.cpp
@@ -4,3 +4,20 @@
  * Implements the test 6.22 class
  * 练习 6.22: 编写一个函数， 令其交换两个 int 指针。
  *===============================================================================================**/
+#include<iostream>
+#include<string>
+using namespace std;
+void  exchange(int* a, int* b) {
+	int* temp;
+	temp = a;
+	a = b;
+	b = temp;
+	cout <<"交换后："<<" "<< *a << " " << *b << endl;
+}
+int main() {
+	cout << "请输入2个数字：" << endl;
+	int num1, num2;
+	cin >> num1 >> num2;
+	cout << "交换前：" << " " << num1 << " " << num2 << endl;
+	exchange(&num1, &num2);
+}
