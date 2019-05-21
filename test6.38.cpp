01d842954dc857a8366f173e782a92a549e5a0d0
zxy3 <30621467+zxy3@users.noreply.github.com>
2019-05-21 16:10:20 +0800

    提交更改test6.37-test6.38

--- test6.38.cpp
+++ test6.38.cpp
@@ -5,8 +5,8 @@
  * 练习 6.38: 修改 arrPtr 函数, 使其返回数组的引用。
  *===============================================================================================**/
 
-//int odd[]{ 1,3,5,7,9 };
-//int even[]= { 0,2,4,6,8 };
-//decltype(odd) SarrPtr(int i) {
-//	return (i % 2) ? odd : even;
-//}
+int odd[]{ 1,3,5,7,9 };
+int even[]= { 0,2,4,6,8 };
+decltype(odd) SarrPtr(int i) {
+	return (i % 2) ? odd : even;
+}
